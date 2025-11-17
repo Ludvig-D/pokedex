1ea944cc0dbfc9ad101cf9d0c8c33940d5c86809
Ludvig <74721111+Ludvig-D@users.noreply.github.com>
2025-11-17 13:38:20 +0100

    Fetching pokemons

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,24 @@
+import { useEffect, useState, useRef } from 'react';
 import './App.css';
 
 function App() {
+  const [pokemons, setPokemons] = useState([]);
+  const hasFetched = useRef(false);
+
+  useEffect(() => {
+    if (hasFetched.current) return;
+    hasFetched.current = true;
+    async function fetchPokemons() {
+      const response = await fetch(
+        'https://pokeapi.co/api/v2/pokemon?limit=20&offset=0'
+      );
+      const newPokemons = await response.json();
+      setPokemons((prev) => [...prev, ...newPokemons.results]);
+    }
+    fetchPokemons();
+  }, []);
+
+  console.log(pokemons);
   return <></>;
 }
 
